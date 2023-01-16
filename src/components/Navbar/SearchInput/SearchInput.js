be0a9ca6c9bd9040dc103bd50f8e6a21cfb4a60f
Ethan Dann <etdann01@gmail.com>
2023-01-16 15:55:24 -0600

    deleted commented code

--- src/components/Navbar/SearchInput/SearchInput.js
+++ src/components/Navbar/SearchInput/SearchInput.js
@@ -53,20 +53,9 @@ const SearchInput = (props) => {
   const [results, setResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [showResults, setShowResults] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  // useEffect(() => {
-  //   axios
-  //     .get(`https://api.coingecko.com/api/v3/coins/list`)
-  //     .then(({ data }) => {
-  //       setCoins(data);
-  //     });
-  //   //eslint-disable-next-line
-  // }, [coins]);
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
     if (e.target.value.length > 0) {
-      // getResults(e.target.value);
       const filteredResults = coins.filter((coin) =>
         coin.name.toLowerCase().includes(e.target.value.toLowerCase())
       );
